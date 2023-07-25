3a58f0d0d1117c448d772ddcad8c543ef1653d35
aaroncherian <aaron.cherian2@gmail.com>
2023-07-25 17:07:24 -0400

    adding result plots

--- NIH_analyses/path_length_line_plots.py
+++ NIH_analyses/path_length_line_plots.py
@@ -8,6 +8,9 @@ import json
 import numpy as np
 import seaborn as sns
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 def aggregate_path_lengths(analysis_folders):
     # Lists to store path lengths
     freemocap_path_lengths = []
@@ -30,8 +33,9 @@ def aggregate_path_lengths(analysis_folders):
             continue
 
         # Read the JSON data
-        json_data = json.load(open(json_path))
-
+        with open(json_path, 'r') as file:
+            json_data = json.load(file)
+        
         # Extract and store path lengths (assuming JSON structure is known)
         if 'sesh' in str(path_to_analysis_folder).lower(): #this is a freemocap folder
             freemocap_path_lengths.append(pd.DataFrame(json_data['Path Lengths:'], index=[0]))
@@ -42,16 +46,7 @@ def aggregate_path_lengths(analysis_folders):
     freemocap_path_lengths = pd.concat(freemocap_path_lengths)
     qualisys_path_lengths = pd.concat(qualisys_path_lengths)
 
-    diff_df = freemocap_path_lengths - qualisys_path_lengths
-
-    # Calculate mean and standard deviation
-    freemocap_mean = diff_df.mean()
-    freemocap_std = diff_df.std()
-
-    qualisys_mean = qualisys_path_lengths.mean()
-    qualisys_std = qualisys_path_lengths.std()
-
-    return freemocap_mean, freemocap_std, qualisys_mean, qualisys_std, diff_df, qualisys_path_lengths
+    return freemocap_path_lengths, qualisys_path_lengths
 
 # Example usage
 analysis_folders = [
@@ -64,49 +59,59 @@ analysis_folders = [
     # ... more paths
 ]
 
-
-freemocap_mean, freemocap_std, qualisys_mean, qualisys_std, freemocap_path_lengths, qualisys_path_lengths = aggregate_path_lengths(analysis_folders)
+freemocap_path_lengths, qualisys_path_lengths = aggregate_path_lengths(analysis_folders)
 
 sns.set_style('whitegrid')
+
+# Calculate mean and standard deviation
+freemocap_mean = freemocap_path_lengths.mean()
+freemocap_std = freemocap_path_lengths.std()
+
+qualisys_mean = qualisys_path_lengths.mean()
+qualisys_std = qualisys_path_lengths.std()
+
 # Conditions
 conditions = freemocap_mean.index
 conditions = [condition.replace('/', '\n') for condition in conditions]
-f = 2
+
 # Create figure with subplots
-fig, axs = plt.subplots(1, 2, figsize=(10, 10), sharey=True)
+fig = make_subplots(rows=1, cols=2, subplot_titles=("Freemocap", "Qualisys"), shared_yaxes=True)
 
-# Plotting Freemocap data
-# for index, row in freemocap_path_lengths.iterrows():
-#     axs[0].plot(conditions, row, '-o', color='#4d7197', alpha=0.6)
+# Add individual lines for Freemocap
+for index, row in freemocap_path_lengths.iterrows():
+    fig.add_trace(
+        go.Scatter(x=conditions, y=row, mode='lines+markers', line=dict(color='#7994B0', width=0.5), showlegend=False),
+        row=1, col=1
+    )
 
-# Adding Freemocap mean and error bars
-axs[0].errorbar(conditions, freemocap_mean, yerr=freemocap_std, fmt='-o', color='black', capsize=5, label='Mean')
+# Add mean line with error bars for Freemocap
+fig.add_trace(
+    go.Scatter(x=conditions, y=freemocap_mean, mode='lines+markers', error_y=dict(type='data', array=freemocap_std, visible=True),
+               line=dict(color='black'), name='Mean'),
+    row=1, col=1
+)
 
-# Labels and titles for Freemocap
-axs[0].set_title('Freemocap', fontsize = 16)
-axs[0].set_ylabel('Normalized Path Length (mm)', fontsize = 14)
-axs[0].legend(loc = 'upper left')
-axs[0].set_xlabel('Condition', fontsize = 14)
-
-# Plotting Qualisys data
+# Add individual lines for Qualisys
 for index, row in qualisys_path_lengths.iterrows():
-    axs[1].plot(conditions, row, '-o', color='#bf6431', alpha=0.6)
+    fig.add_trace(
+        go.Scatter(x=conditions, y=row, mode='lines+markers', line=dict(color='#C67548', width=0.5), showlegend=False),
+        row=1, col=2
+    )
 
-# Adding Qualisys mean and error bars
-axs[1].errorbar(conditions, qualisys_mean, yerr=qualisys_std, fmt='-o', color='black', capsize=5, label='Mean')
+# Add mean line with error bars for Qualisys
+fig.add_trace(
+    go.Scatter(x=conditions, y=qualisys_mean, mode='lines+markers', error_y=dict(type='data', array=qualisys_std, visible=True),
+               line=dict(color='black'), name='Mean'),
+    row=1, col=2
+)
 
-# Labels and titles for Qualisys
-axs[1].set_title('Qualisys', fontsize = 16)
-axs[1].set_xlabel('Condition', fontsize = 14)
-# axs[1].set_ylabel('Normalized Path Length (mm)')
-axs[1].legend(loc = 'upper left')
+# Update layout
+fig.update_layout(height=600, width=700, title_text="Path Length Comparison", template = 'plotly_white')
+fig.update_yaxes(title_text="Path Length (mm)", row=1, col=1)
+fig.update_xaxes(title_text="Condition", row=1, col=1)
+fig.update_xaxes(title_text="Condition", row=1, col=2)
 
-axs[0].set_ylim([0, .1])
-# axs[1].set_ylim([.1, .6])
+# Show figure
+# fig.show()
 
-fig.suptitle('FreeMoCap and Qualisys Normalized Path Lengths vs. Condition', fontsize=16)
-
-
-# Display the plot
-plt.tight_layout()
-plt.show()
+fig.write_html(str(r'C:\Users\aaron\Documents\GitHub\nih_balance_analyses\docs\images\path_length_line_plots.html'), full_html=False, include_plotlyjs='cdn')
---
+++ docs/results.md
@@ -0,0 +1,14 @@
+## Results 
+
+### Center of mass dispersion
+
+The 2d ground plane projection of the center of mass shows increased dispersion during progressively harder stances.
+
+<iframe width="650" height="650" frameborder="0" scrolling="no" src="../images/com_dispersion_plots.html" ></iframe>
+
+### Normalized path length
+
+Center of mass path length shows a generally increasing trend across stances.
+
+<iframe width="700" height="650" frameborder="0" scrolling="no" src='../images/path_length_line_plots.html'> </iframe>
+
