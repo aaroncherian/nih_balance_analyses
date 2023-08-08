7e63308133e287ea4ce234f63d78e87c1308cd32
aaroncherian <aaron.cherian2@gmail.com>
2023-08-08 16:28:51 -0400

    cleaning up frame marking

--- freemocap_utils/GUI_widgets/NIH_widgets/frame_marking_widget.py
+++ freemocap_utils/GUI_widgets/NIH_widgets/frame_marking_widget.py
@@ -16,20 +16,30 @@ class FrameMarker(QWidget):
     """
 
     conditions_dict_updated_signal = pyqtSignal()
+
     def __init__(self):
         super().__init__()
 
         self._layout = QHBoxLayout()
         self.setLayout(self._layout)
-
         self.condition_widget_dictionary = {}
-    
-        conditions_layout = QVBoxLayout()
-        self.conditions_box= QComboBox()
+        self.setup_ui()
 
+    def setup_ui(self):
+        """Set up the user interface elements."""
+        conditions_layout = self.create_conditions_layout()
+        self._layout.addLayout(conditions_layout)
+
+        self.saved_conditions_table = self.create_conditions_table()
+        self._layout.addWidget(self.saved_conditions_table)
+
+    def create_conditions_layout(self):
+        """Create and return the conditions layout."""
+        conditions_layout = QVBoxLayout()
+
+        self.conditions_box = QComboBox()
         self.conditions_box.addItems(['Eyes Open/Solid Ground', 'Eyes Closed/Solid Ground', 'Eyes Open/Foam', 'Eyes Closed/Foam'])
         self.conditions_box.currentTextChanged.connect(self.reset_start_and_end_frames)
-        
         conditions_layout.addWidget(self.conditions_box)
 
         text_width = 100
@@ -44,9 +54,8 @@ class FrameMarker(QWidget):
         frame_form = QFormLayout()
         frame_form.addRow(QLabel('Start Frame'), self.starting_frame)
         frame_form.addRow(QLabel('End Frame'), self.ending_frame)
-
         conditions_layout.addLayout(frame_form)
-        
+
         self.save_condition_button = QPushButton('Save Frame Interval For This Condition')
         self.save_condition_button.pressed.connect(self.save_conditions_to_table)
         self.save_condition_button.setEnabled(False)
@@ -62,42 +71,55 @@ class FrameMarker(QWidget):
         self.interval_input.setFixedWidth(text_width)
 
         self.auto_set_end_frame_checkbox = QCheckBox("Auto-Fill End Frame with Interval")
+        self.auto_set_end_frame_checkbox.stateChanged.connect(self.calculate_ending_frame)
+        self.starting_frame.textChanged.connect(self.calculate_ending_frame)
 
         interval_layout = QHBoxLayout()
         interval_layout.addWidget(self.auto_set_end_frame_checkbox)
         interval_layout.addWidget(self.interval_input)
-
         conditions_layout.addLayout(interval_layout)
 
-        # Connect the textChanged signal to calculate the ending frame
-        self.starting_frame.textChanged.connect(self.calculate_ending_frame)
+        return conditions_layout
+    
+    def create_conditions_table(self):
+        """Create and return the conditions table."""
+        table = QTableWidget()
+        table.setRowCount(4)
+        table.setColumnCount(2)
+        return table
+    
+    def update_conditions_table(self):
+        """Update the conditions table with current data."""
+        self.saved_conditions_table.setVerticalHeaderLabels(list(self.condition_widget_dictionary.keys()))
 
-        self._layout.addLayout(conditions_layout)
-
-        self.saved_conditions_table = QTableWidget()
-        self.saved_conditions_table.setRowCount(4)
-        self.saved_conditions_table.setColumnCount(2)
-        self._layout.addWidget(self.saved_conditions_table)
+        for row, condition in enumerate(self.condition_widget_dictionary.keys()):
+            start_frame, end_frame = self.condition_widget_dictionary[condition]
+            self.saved_conditions_table.setItem(row, 0, QTableWidgetItem(str(start_frame)))
+            self.saved_conditions_table.setItem(row, 1, QTableWidgetItem(str(end_frame)))
 
     def save_conditions_to_table(self):
         current_condition = self.conditions_box.currentText()
         start_frame = int(self.starting_frame.text())
         end_frame = int(self.ending_frame.text())
 
-        self.condition_widget_dictionary[current_condition] = [start_frame,end_frame]
-        self.saved_conditions_table.setVerticalHeaderLabels(list(self.condition_widget_dictionary.keys()))
-
-
-        for row_count, condition in enumerate(self.condition_widget_dictionary.keys()):
-            this_start_frame = self.condition_widget_dictionary[condition][0]
-            this_end_frame = self.condition_widget_dictionary[condition][1]
-            self.saved_conditions_table.setItem(row_count,0,QTableWidgetItem(str(this_start_frame)))
-            self.saved_conditions_table.setItem(row_count,1,QTableWidgetItem(str(this_end_frame)))
+        self.condition_widget_dictionary[current_condition] = [start_frame, end_frame]
+        self.update_conditions_table()
 
         self.conditions_dict_updated_signal.emit()
 
+    def load_conditions(self):
+        self.condition_json, _ = QFileDialog.getOpenFileName(self, "Select JSON file", "", "JSON Files (*.json)")
+
+        if self.condition_json:
+            with open(self.condition_json, 'r') as json_file:
+                data = json.load(json_file)
+
+            self.condition_widget_dictionary = data['Frame Intervals']
+            self.update_conditions_table()
+
+            self.conditions_dict_updated_signal.emit()
+
     def calculate_ending_frame(self):
-        """Calculate the ending frame based on the starting frame and interval."""
         if self.auto_set_end_frame_checkbox.isChecked() and self.starting_frame.text() and self.interval_input.text():
             try:
                 starting = int(self.starting_frame.text())
@@ -106,34 +128,11 @@ class FrameMarker(QWidget):
             except ValueError:
                 # Handle any potential conversion errors if the input isn't a valid integer yet
                 pass
-    
-    def load_conditions(self):
-        self.folder_diag = QFileDialog()
-        self.condition_json, _ = QFileDialog.getOpenFileName(self, "Select JSON file", "", "JSON Files (*.json)")
-
-        if self.condition_json:
-            with open(self.condition_json, 'r') as json_file:
-                data = json.load(json_file)
-        
-            self.condition_widget_dictionary = data['Frame Intervals']
-            
-            for row_count, condition in enumerate(self.condition_widget_dictionary.keys()):
-                this_start_frame = self.condition_widget_dictionary[condition][0]
-                this_end_frame = self.condition_widget_dictionary[condition][1]
-                self.saved_conditions_table.setItem(row_count,0,QTableWidgetItem(str(this_start_frame)))
-                self.saved_conditions_table.setItem(row_count,1,QTableWidgetItem(str(this_end_frame)))
-
-            self.conditions_dict_updated_signal.emit()
-        f = 2 
-
 
     def reset_start_and_end_frames(self):
         self.starting_frame.setText(None)
-        self.ending_frame.setText(None)
+        self.ending_frame.setText(None)    
 
-
-        f = 2
-
-    
+        
 
 
